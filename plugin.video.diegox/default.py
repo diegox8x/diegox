dcd50291a6883f6997dc5b90dd2c6c49dfc5562d
diegox8x <felipin36@hotmail.com>
2025-02-19 23:36:34 -0500

    plu

--- plugin.video.diegox/default.py
+++ plugin.video.diegox/default.py
@@ -1,17 +1,35 @@
+import sys
 import xbmcplugin
 import xbmcgui
-import sys
+import xbmcaddon
+import xbmc
 
-# Obtiene el handle del plugin
-handle = int(sys.argv[1])
+# Identificación del addon
+addon_handle = int(sys.argv[1])
+xbmcplugin.setContent(addon_handle, 'videos')
 
-# Crea una lista de elementos para el menú
-def show_menu():
-    url = "plugin://plugin.video.diegox/play"
-    list_item = xbmcgui.ListItem(label="Reproducir Video")
-    xbmcplugin.addDirectoryItem(handle, url, list_item, isFolder=False)
+# Crear menú de opciones
+def build_menu():
+    url = sys.argv[0] + "?action=play"
+    li = xbmcgui.ListItem("Reproducir Video de Prueba")
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=False)
+    xbmcplugin.endOfDirectory(addon_handle)
 
-    xbmcplugin.endOfDirectory(handle)
+# Procesar acciones
+def router(paramstring):
+    if "action=play" in paramstring:
+        play_video()
+    else:
+        build_menu()
 
-# Ejecuta la función
-show_menu()
+# Función para reproducir un video
+def play_video():
+    url = "https://www.example.com/video.mp4"  # URL de prueba
+    li = xbmcgui.ListItem("Video de prueba")
+    li.setInfo('video', {'title': 'Video de prueba'})
+    li.setPath(url)
+    xbmc.Player().play(url, li)
+
+# Ejecutar router
+if __name__ == '__main__':
+    router(sys.argv[2])
